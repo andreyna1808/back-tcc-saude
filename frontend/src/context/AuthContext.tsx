4be1173db85665131751890fb4a9cfae8cbf717d
Andreyna Carvalho <andreyna.carvalho@codex.com.br>
2024-09-03 09:35:13 -0300

    feat: update chackra provider and apply default theme

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,79 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+interface AuthContextProps {
+  token: string | null;
+  expiresAt: number | null;
+  typeUser: string | null;
+  login: (newToken: string, expiresAt: number, typeUser: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [token, setToken] = useState<string | null>(null);
+  const [typeUser, setTypeUser] = useState<string | null>(null);
+  const [expiresAt, setExpiresAt] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedToken = localStorage.getItem("authToken");
+      const storedExpiresAt = localStorage.getItem("authExpiresAt");
+      const storedTypeUser = localStorage.getItem("typeUser");
+
+      if (storedToken) setToken(storedToken);
+      if (storedExpiresAt) setExpiresAt(parseInt(storedExpiresAt));
+      if (storedTypeUser) setTypeUser(storedTypeUser);
+    }
+  }, []);
+
+  const login = (newToken: string, expiresAt: number, typeUser: string) => {
+    setToken(newToken);
+    setExpiresAt(expiresAt);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("authToken", newToken);
+      localStorage.setItem("typeUser", typeUser);
+      localStorage.setItem("authExpiresAt", expiresAt.toString());
+    }
+  };
+
+  const logout = () => {
+    setToken(null);
+    setExpiresAt(null);
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("authExpiresAt");
+      localStorage.removeItem("typeUser");
+    }
+  };
+
+  useEffect(() => {
+    if (expiresAt) {
+      const timeout = expiresAt - Date.now();
+      if (timeout > 0) {
+        const timer = setTimeout(() => {
+          logout();
+        }, timeout);
+        return () => clearTimeout(timer);
+      } else {
+        logout();
+      }
+    }
+  }, [expiresAt]);
+
+  return (
+    <AuthContext.Provider value={{ token, expiresAt, typeUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
